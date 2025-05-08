4d07bec66e10f56dc7c336c740f4175eb0b1ef66
Sankadeepa_IM <imashsankadeepa27@gmail.com>
2025-05-09 00:02:59 +0530

    updateItem.css  and js file

--- frontend/src/components/UpdateItem/UpdateItem.js
+++ frontend/src/components/UpdateItem/UpdateItem.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router'  
+import { useParams } from 'react-router' 
+import './UpdateItem.css' 
 
 
 function UpdateItem() {
